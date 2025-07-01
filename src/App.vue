0b8685cd95b14703dec122408f8eea7b0acf32a6
retupmoc258 <retupmoc258@gmail.com>
2025-07-01 16:56:36 -0600

    Prepares application for development.  Adds a navbar (no real functionality, yet).

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <NavBar/>
 
   <RouterView />
 </template>
@@ -26,10 +15,6 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 nav {
   width: 100%;
@@ -63,10 +48,6 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
   header .wrapper {
     display: flex;
     place-items: flex-start;
@@ -75,11 +56,9 @@ nav a:first-of-type {
 
   nav {
     text-align: left;
-    margin-left: -1rem;
     font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    margin: 0;
+    padding-bottom: 1rem;
   }
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <RouterLink to="/" class="navbar-brand">Jason Bagley</RouterLink>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <RouterLink to="/" class="nav-link">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">About</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/contact" class="nav-link">Contact</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+
+</script>
+
+<style scoped>
+/* Add your CSS styling for the navbar here */
+
+.navbar {
+  top: 0;
+  left: 0;
+  width: 100%;
+  position: fixed;
+  z-index: 200;
+}
+.container {
+  background-color: #333;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.navbar-brand {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+.navbar-nav {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+.nav-item {
+  margin-left: 1rem;
+}
+.nav-link {
+  color: white;
+  text-decoration: none;
+}
+.router-link-active {
+  font-weight: bold;
+  /* Add styling for active link */
+}
+</style>
