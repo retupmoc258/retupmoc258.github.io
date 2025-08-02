ba1b008e9497c4368b2e82121a985b747fb3f0fc
retupmoc258 <retupmoc258@gmail.com>
2025-08-02 17:35:13 -0600

    Adds ContactFooter.vue reusable component to add to all views.  Adds Projects component to enable new view soon.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,52 +2,21 @@
   <div class="portfolio-view">
     <Hero/>
 
-
-
     <!-- About Section -->
     <section class="detail-section">
       <h2>About Me</h2>
       <p class="ml-5">I graduated from Western Governors University in June 2025.  I have been developing small software projects for work since 2020.
-        I love learning, teaching, and playing board games and video games.  I am currently testing my YML.</p>
+        I love learning, teaching, and playing board games and video games.</p>
     </section>
 
     <!-- Skills Condensed -->
     <Skills condensed />
 
     <!-- Projects Section -->
-    <section class="projects-section">
-      <h2>My Projects</h2>
-      <div v-for="project in projects" :key="project.id" class="projects-section">
-
-        <div class="green">
-          <p v-if="project.link === ''" class="green" >{{ project.title }}</p>
-          <p v-else-if="project.download"><a :href="project.link" target="_blank" download>{{ project.title }}</a></p>
-          <p v-else><a :href="project.link" target="_blank">{{ project.title }}</a></p>
-        </div>
-
-
-
-
-        <p class="project-description">{{ project.description }}</p>
-      </div>
-    </section>
-
-    <!--<section class="projects-section">
-      <h2>Current Passion Project</h2>
-      <div v-for="project in projects" :key="project.id" class="projects-section">
-        <p class="green" >HIPAA compliant Student Information System (SIS)</p>
-        <p class="project-description">{{ project.description }}</p>
-      </div>
-    </section>  -->
+    <Projects />
 
     <!-- Contact Section -->
-    <section class="contact-section">
-      <h2>Get in Touch</h2>
-      <p>Email: <a href="mailto:retupmoc258@gmail.com">retupmoc258@gmail.com</a></p>
-      <!-- Optional: Add links to your social media profiles -->
-      <!-- <a href="https://linkedin.com/in/yourprofile" target="_blank">LinkedIn</a> -->
-      <a href="https://github.com/retupmoc258" target="_blank">GitHub</a>
-    </section>
+    <ContactFooter/>
   </div>
 </template>
 
@@ -55,80 +24,12 @@
 import Hero from "@/components/Hero.vue";
 import SkillsView from "@/views/SkillsView.vue";
 import Skills from "@/components/Skills.vue";
+import ContactFooter from "@/components/ContactFooter.vue";
+import Projects from "@/components/Projects.vue";
 
 export default {
-  name: 'PortfolioView',
-  components: {Skills, SkillsView, Hero},
-  data() {
-    return {
-      // You can define data for your projects here, or fetch it from an API
-      projects: [
-        {
-          id: 0,
-          title: 'This website.',
-          description: 'The website you are currently viewing was created using Vue.js and Tailwind CSS.',
-          link: '',
-          download: false
-        },
-        {
-          id: 1,
-          title: 'Vue Crash Course project (by Brad Traversy)',
-          description: 'This was an application I made based on Brad Traversy\'s "Vue Crash Course" YouTube video.  ' +
-              'Project is free to use.  More information about the video can be found in the README.md file',
-          link: 'https://github.com/retupmoc258/vue-project',
-          download: false
-        },
-        {
-          id: 2,
-          title: 'Software Engineering Capstone (Vacation Planner mobile application v 1.0.1)',
-          description: 'This was my capstone project for software engineering.  I created a mobile application that allows a user to track vacation information.' +
-              ' The application was designed for Android and uses Java as its programming language.  It also uses the Room framework to use the underlying' +
-              ' database structures securely.  The application is available to use for free, if desired, but may not be copied, redistributed, or modified.' +
-              ' Any use of this software for gain (including submitting for a job or school course) is prohibited.',
-          link: '/files/vacation_planner_1.0.1.apk',
-          download: true
-        },
-        {
-          id: 3,
-          title: 'Java Fullstack Inventory Application',
-          description: 'This was a school project for Java Frameworks.  The application was designed to allow a user to manage an inventory of items.' +
-              ' The application was designed for Java and uses the Spring Framework to create the backend.' +
-              ' The link will take you to my repository, where you may download the source code.  Full instructions are in the README.txt file.',
-          link: 'https://github.com/retupmoc258/Java-Fullstack-Inventory-Application',
-          download: false
-        },
-        {
-          id: 4,
-          title: '"World Map" Angular Project with World Bank API',
-          description: 'This school project focuses on using the Angular framework to create a basic web application.  ' +
-              'A world map is displayed and when you click on a country, it uses the World Bank API to retrieve ' +
-              'information about that country and display it on the web page.  The repository is private to prevent students from copying the code.  ' +
-              'If you would like to see the repository code or test the application, please email me.',
-          link: '',
-          download: false
-        },
-        {
-          id: 5,
-          title: 'User Interface Design project (Taniti)',
-          description: 'This project was to create a user interface design that would be friendly for a user.' +
-              ' This project was designed for a mobile device, and the age will simulate the view on a mobile device.' +
-              ' I created the interface, but I did not program the HTML elements.  This project uses Justinmind.',
-          link: 'taniti/index.html',
-          download: false
-        },
-      ]
-    };
-  },
-  // Optional: Add methods for fetching data or other logic
-  // methods: {
-  //   fetchProjects() {
-  //     // Fetch project data from an API
-  //   }
-  // },
-  // Optional: Fetch data when the component is mounted
-  // mounted() {
-  //   this.fetchProjects();
-  // }
+  name: 'HomeView',
+  components: {Projects, ContactFooter, Skills, SkillsView, Hero},
 };
 </script>
 
@@ -143,15 +44,10 @@ export default {
   align-self: stretch;
 }
 
-.detail-section,
-.projects-section,
-.contact-section {
+.detail-section {
   margin-bottom: 20px;
 }
 
-.project-description {
-  margin-left: 20px;
-}
 
 
 /* Add more styles as needed */
