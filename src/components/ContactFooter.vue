d6b2e6c3155a19037983c7c9381aaa6b9f9bc5ad
retupmoc258 <retupmoc258@gmail.com>
2025-08-02 18:14:07 -0600

    Adds color to links to help identify links.

--- src/components/ContactFooter.vue
+++ src/components/ContactFooter.vue
@@ -5,10 +5,10 @@
 <template>
   <section class="mb-5">
     <h2>Get in Touch</h2>
-    <p>Email: <a href="mailto:retupmoc258@gmail.com">retupmoc258@gmail.com</a></p>
+    <p>Email: <a class="green" href="mailto:retupmoc258@gmail.com">retupmoc258@gmail.com</a></p>
     <!-- Optional: Add links to your social media profiles -->
-    <p>GitHub: <a href="https://github.com/retupmoc258" target="_blank">GitHub</a></p>
-    <p>LinkedIn: <a href="www.linkedin.com/in/jason-bagley-168b6429a" target="_blank">LinkedIn</a></p>
+    <p>GitHub: <a class="green" href="https://github.com/retupmoc258" target="_blank">GitHub</a></p>
+    <p>LinkedIn: <a class="green" href="www.linkedin.com/in/jason-bagley-168b6429a" target="_blank">LinkedIn</a></p>
   </section>
 </template>
 
